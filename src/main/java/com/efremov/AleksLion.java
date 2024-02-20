16164be05207150f68f8489206b68bea9418d6fc
eefremov <axleevgeney@gmail.com>
2024-02-20 22:44:03 +0300

    задание со звездочкой исправления

--- src/main/java/com/efremov/AleksLion.java
+++ src/main/java/com/efremov/AleksLion.java
@@ -1,16 +1,31 @@
 package com.efremov;
 
+import java.util.List;
+
 public class AleksLion extends Lion {
 
-    private int roarVolume;
+    private final List<String> friends;
+    private final String placeOfLiving;
 
-    public AleksLion(String sex, CatFamily feline, int roarVolume) throws Exception {
-        super(sex, feline);
-        this.roarVolume = roarVolume;
+    public AleksLion() throws Exception {
+        super("Самец", null); // В данном случае передаем null, так как CatFamily не используется
+        this.friends = List.of("Марти", "Глория", "Мелман");
+        this.placeOfLiving = "Нью-Йоркский зоопарк";
     }
 
-    public int getRoarVolume() {
-        return roarVolume;
+
+    @Override
+    public int getKittens() {
+        return 0;
+    }
+
+    public List<String> getFriends() {
+        return friends;
+    }
+
+    public String getPlaceOfLiving() {
+        return placeOfLiving;
     }
 }
+
 
--- src/test/java/AleksLionTest.java
+++ src/test/java/AleksLionTest.java
@@ -4,48 +4,27 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class AleksLionTest {
 
     @Test
-    public void testAleksLionConstructorMale() throws Exception {
-        CatFamily mockCatFamily = mock(CatFamily.class);
-        when(mockCatFamily.getKittens()).thenReturn(3);
-        when(mockCatFamily.doesHaveMane()).thenReturn(true);
-        when(mockCatFamily.getFood()).thenReturn(List.of("Meat"));
-
-        AleksLion aleksLion = new AleksLion("Самец", mockCatFamily, 5);
-
-        assertEquals(3, aleksLion.getKittens());
-        assertTrue(aleksLion.doesHaveMane());
-        assertEquals(List.of("Meat"), aleksLion.getFood());
-        assertEquals(5, aleksLion.getRoarVolume());
+    public void testAleksLionGetKittens() throws Exception {
+        AleksLion aleksLion = new AleksLion();
+        assertEquals(0, aleksLion.getKittens());
     }
 
     @Test
-    public void testAleksLionConstructorFemale() throws Exception {
-        CatFamily mockCatFamily = mock(CatFamily.class);
-        when(mockCatFamily.getKittens()).thenReturn(4);
-        when(mockCatFamily.doesHaveMane()).thenReturn(false);
-        when(mockCatFamily.getFood()).thenReturn(List.of("Meat"));
-
-        AleksLion aleksLion = new AleksLion("Самка", mockCatFamily, 8);
-
-        assertEquals(4, aleksLion.getKittens());
-        assertFalse(aleksLion.doesHaveMane());
-        assertEquals(List.of("Meat"), aleksLion.getFood());
-        assertEquals(8, aleksLion.getRoarVolume());
+    public void testAleksLionGetFriends() throws Exception {
+        AleksLion aleksLion = new AleksLion();
+        assertEquals(List.of("Марти", "Глория", "Мелман"), aleksLion.getFriends());
     }
 
     @Test
-    public void testAleksLionConstructorInvalidSex() {
-        CatFamily mockCatFamily = mock(CatFamily.class);
-
-        Throwable exception = assertThrows(Exception.class, () -> new AleksLion("InvalidSex", mockCatFamily, 10));
-
-        assertEquals("Используйте допустимые значения пола животного - самец или самка", exception.getMessage());
+    public void testAleksLionGetPlaceOfLiving() throws Exception {
+        AleksLion aleksLion = new AleksLion();
+        assertEquals("Нью-Йоркский зоопарк", aleksLion.getPlaceOfLiving());
     }
 }
